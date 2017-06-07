6d72f9d4be2dbba1c11d2412aabc3bf72368dbfa
nitharshini <nitharshinis094@gmail.com>
2017-06-07 10:24:14 -0700

    Update kalman_filter.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,6 @@
 #include "kalman_filter.h"
+#include "tools.h"
+#include<math.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -22,6 +24,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +34,20 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_* P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +55,35 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+    MatrixXd Hj(3,4);
+
+    double px = x_(0);
+    double py = x_(1);
+    double vx = x_(2);
+    double vy = x_(3);
+
+    double sum = (px*px)+(py*py);
+    double rho = sqrt(sum);
+    double phi = atan2(px,py);
+    double rho_dot = ((px*vy)+(py*vx))/rho;
+
+    //Hj = Tools().CalculateJacobian(x_);
+
+    VectorXd z_pred(3);
+    z_pred << rho,phi,rho_dot;
+
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = Hj.transpose();
+	MatrixXd S = Hj * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * Hj) * P_;
+
 }
